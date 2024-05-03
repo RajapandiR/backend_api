c5eaa7fca65ac963b2be7a0df8d839ff07a62a1d
RajapndiR <rajapndi.r@quadkast.co.in>
2024-05-03 18:05:32 +0530

    bug fixed

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken"
 
 class JwtClass {
     issueToken(payload: any) {
-        return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '2m' });
+        return jwt.sign(payload, process.env.TOKEN_SECRET!, { expiresIn: '2m' });
     }
     verifyToken(payload: any) {
-        return jwt.verify(payload, process.env.TOKEN_SECRET)
+        return jwt.verify(payload, process.env.TOKEN_SECRET!)
     }
 }
 
